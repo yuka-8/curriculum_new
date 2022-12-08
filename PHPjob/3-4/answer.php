214bd9dc9eeac04405960c31a0543f55a203376a
yuka-8 <itmsck15outlook.jp>
2022-12-08 17:47:36 +0900

    課題提出テスト
    課題の作成

--- PHPjob/3-4/answer.php
+++ PHPjob/3-4/answer.php
@@ -12,24 +12,24 @@
    $question3 = $_POST["question3"];
    $answer3 = $_POST["answer3"];
 
-   $question = ["question1","question2","question3"];
-   $answer = ["answer1","answer2","answer3"];
-   $result = ["result1","result2","result3"];
-
-   while ($question == $answer){
-    $result = "正解！";
-    }else{
-    $result = "残念・・";
-    }
+   function comparisonAnswer($question,$answer){
+      if($question == $answer) {
+         echo "正解";
+      }else{
+         echo "残念";      
+      }
+   }
 ?>
 
 <p><?php echo $my_name; ?>さんの結果は・・・？</p>
 
    <p>①の答え</p>
-   <?php echo $result1; ?>
+   <?php comparisonAnswer($question1,$answer1); ?>
 
    <p>②の答え</p>
-   <?php echo $result2; ?>
+   <?php comparisonAnswer($question2,$answer2); ?>
+
 
    <p>③の答え</p>
-   <?php echo $result3; ?>
+   <?php comparisonAnswer($question3,$answer3); ?>
+
